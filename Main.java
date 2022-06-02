65e7fa016ab71b3127ff934148c6933c23e496dd
ssongK <73736882+ssongK@users.noreply.github.com>
2022-06-02 18:18:57 +0900

    220602

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int c[] = new int[3];
+        int result = 0;
+        c[2]=b/100;
+        b%=100;
+        c[1]=b/10;
+        b%=10;
+        c[0]=b;
+        for(int i=0;i<c.length;i++){
+            c[i]=a*c[i];
+            System.out.println(c[i]);
+        }
+        result = c[0]+c[1]*10+c[2]*100;
+        System.out.println(result);
+        s.close();
+    }    
+}
