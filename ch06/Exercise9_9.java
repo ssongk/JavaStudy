d183cc96bc8adaa525c18beb0b1dba40b64efb95
ssongK <73736882+ssongK@users.noreply.github.com>
2022-06-15 19:59:41 +0900

    220614

--- ch06/Exercise9_9.java
+++ ch06/Exercise9_9.java
@@ -1,18 +1,14 @@
 package ch06;
+
 public class Exercise9_9 {
     static String delChar(String src, String delCh){
         StringBuffer sb = new StringBuffer();
         for(int i=0;i<src.length();i++){
-            int toggle = 0;
-            char c = src.charAt(i);
-            for(int k=0;k<delCh.length();k++){
-                if(src.charAt(i)==delCh.charAt(k)){
-                    toggle++;
-                }   
-            }
-            if(toggle==0)
-                sb.append(Character.toString(c));
-        }            
+            String s = Character.toString(src.charAt(i));
+            if(delCh.indexOf(s)==-1){
+                sb.append(s);
+            }   
+        }
         return sb.toString();
     }
     public static void main(String[] args) {
