49dd7c1b32ce80dc2ddfda8e878f093f40974700
ssongK <73736882+ssongK@users.noreply.github.com>
2022-07-13 00:58:30 +0900

    220713

--- DisignPattern/Decorator/LaneDecorator.java
+++ DisignPattern/Decorator/LaneDecorator.java
@@ -4,13 +4,11 @@ public class LaneDecorator extends DisplayDecorator {
     public LaneDecorator(Display decoratedDisplay){
         super(decoratedDisplay);
     }
-    
     @Override
     public void draw(){
         super.draw();
         drawLine();
     }
-
     private void drawLine(){
         System.out.print("+ 차선 표시");
     }
